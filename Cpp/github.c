e64e49c5ba44c9810eaadcef6964e5789864f0fa
kpdotin <karthikpandiyan2712@gmail.com>
2022-04-20 22:56:42 +0530

    first commit

--- Cpp/github.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int bill_amount , amount_given ,  a, b;
-    scanf("%d %d",&bill_amount,&amount_given);
-    a=bill_amount/amount_given;
-    b=bill_amount%amount_given;
-    printf("%d\n%d",a,b);
-}
